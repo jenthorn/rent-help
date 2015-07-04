f43d068e2f94208ee76d3c825fd1819d07e4024a
jenThorn <cjenniferthorn@gmail.com>
2015-07-04 19:26:54 -0400

    Updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
 		browserSync = require('browser-sync'),
 		reload = browserSync.reload,
 		concat = require('gulp-concat'),
+		mincss = require('gulp-minify-css'),
 		sass = require('gulp-sass'),
 		plumber = require('gulp-plumber'),
 		notify = require('gulp-notify'),
@@ -29,6 +30,7 @@ gulp.task('styles', function(){
 	}))
 	.pipe(sass())
 	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+	.pipe(mincss())
 	.pipe(gulp.dest('styles/'))
 	.pipe(reload({stream: true}));
 });
