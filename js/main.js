1899644238dabbf5c1bfb3f3c27af7592c34d72e
jenThorn <cjenniferthorn@gmail.com>
2015-07-07 19:31:09 -0400

    drop down select. added loading gif

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var hood = {};
 
+hood.d = new Date();
+hood.year = hood.d.getFullYear();
+
 hood.collectInfo = function(){
 	$('form').on('submit', function(e){
 		e.preventDefault();
@@ -10,9 +13,7 @@ hood.collectInfo = function(){
 		var price = $('#price').val();
 		var beds = $('#beds').val();
 		hood.getInfo(location, price, beds);
-		var loading = $('<p>').addClass('loading').text('loading');
-		$('.found').html(loading);
-		$('#location').val('');
+		$('.loading').addClass('show');
 	});
 };
 
@@ -25,6 +26,7 @@ hood.getInfo = function(itemOne, itemTwo, itemThree){
 		timeout: 5000,
 		success: function(response){
 			$('.found').empty();
+			$('.loading').removeClass('show');
 			hood.displayInfo(response);
 		},
 		error: function(x, t, m) {
@@ -48,13 +50,18 @@ hood.displayInfo = function(result){
 	});
 
 	$.each(result, function(i, item){
-		if (item.title.indexOf('basement') > 0 || item.title.indexOf('Basement') > 0 || item.title.indexOf('Lower level') > 0 || item.title.indexOf('lower level') > 0 || item.title.indexOf('Wanted') > 0 || item.title.indexOf('WANTED') > 0 || item.title.indexOf('wanted') > 0 ) {
+		if (item.title.indexOf('basement') > 0 || item.title.indexOf('bsmt') > 0 || item.title.indexOf('Bsmt') > 0 || item.title.indexOf('BSMT') > 0 || item.title.indexOf('Basement') > 0 || item.title.indexOf('Lower level') > 0 || item.title.indexOf('lower level') > 0 || item.title.indexOf('Wanted') > 0 || item.title.indexOf('WANTED') > 0 || item.title.indexOf('wanted') > 0 ) {
 			console.log('fuck that');
 		} else {
 			var pic = $('<img>').attr('src', item.pic);
 			var title = $('<h2>').text(item.title);
 			var price = $('<h3>').text(item.price);
-			var posted =$('<p>').text('Date Posted: ' + item.posted);
+
+			
+
+			var now = moment(item.posted).year(hood.year).format("MMM Do, YYYY");
+
+			var posted =$('<p>').text('Date Posted: ' + now);
 			var article = $('<article>').addClass('result').append(pic, title, price, posted);
 			var link = $('<a>').attr({href: item.link,target: '_blank'}).html(article);
 			$('.found').append(link);
@@ -74,10 +81,11 @@ hood.displayInfo = function(result){
 $(function(){
 	console.log('loaded');
 	hood.collectInfo();
+
 	$("#price").ionRangeSlider({
 	  hide_min_max: true,
 	  keyboard: true,
-	  min: 1500,
+	  min: 1000,
 	  max: 3000,
 	  from: 1000,
 	  to: 4000,
